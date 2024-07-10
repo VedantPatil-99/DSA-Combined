aae1940d8eb3432f5b3a6a18d07b377bbf357122
Vedant Patil <137187890+VedantPatil-99@users.noreply.github.com>
2024-07-11 00:00:05 +0530

    Add a Quick Sort Program

--- Learn Sorting/Sorting.py
+++ Learn Sorting/Sorting.py
@@ -65,7 +65,27 @@ def merge(arr, low, mid, high):
 
 
 #------------------ Quick Sort ----------------------------
-
+def quickSort(arr, low, high):
+  if(low < high):
+    pIdx = partition(arr, low, high)
+    quickSort(arr, low, pIdx-1)
+    quickSort(arr, pIdx+1, high)
+    
+def partition(arr, low, high):
+  pivot = arr[low]
+  i = low
+  j = high
+  while(i < j):
+    while(arr[i] <= pivot and i <= high-1):
+      i += 1
+    while(arr[j] > pivot and j >= low+1):
+      j -= 1
+    if i < j:
+      arr[i], arr[j] = arr[j], arr[i]
+  arr[low], arr[j] = arr[j], arr[low]
+  return j
+  
+  
 if __name__ == '__main__':
     arr = [2, 9, 4, 18, 6]
     n = len(arr)
