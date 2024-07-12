df7a991a8e0786b40dac79110da6f0474dfeecac
Vedant Patil <137187890+VedantPatil-99@users.noreply.github.com>
2024-07-12 15:55:53 +0530

    Add 8 new Patterns Patterns.py

--- Learn The Basics/Patterns.py
+++ Learn The Basics/Patterns.py
@@ -124,6 +124,123 @@ def diamond_shape_of_asterisks(n):
     pyramid_of_asterisks(n)
     upside_down_pyramid_of_asterisks(n)
 
+#----------------- Alternating 0 and 1 Pattern -------------------------
+def alternating_binary_pattern(n):
+    """
+    1
+    01
+    101
+    0101
+    10101
+    """
+    for i in range(n):
+        if i % 2 == 0:
+            start = 1
+        else:
+            start = 0
+        for j in range(i + 1):
+            print(start, end="")
+            start = 1 - start
+        print()
+
+#----------------- Number Pyramid with Spaces -------------------------
+def number_pyramid_with_spaces(n):
+    """
+    1               1
+    1 2           2 1
+    1 2 3       3 2 1
+    1 2 3 4   4 3 2 1
+    1 2 3 4 5 5 4 3 2 1
+    """
+    for i in range(1, n + 1):
+        for j in range(1, i + 1):
+            print(j, end=" ")
+        for j in range(1, (2 * (n - i) + 1)):
+            print(" ", end=" ")
+        for j in range(i, 0, -1):
+            print(j, end=" ")
+        print()
+
+#----------------- Incremental Number Triangle -------------------------
+def incremental_number_triangle(n):
+    """
+    1
+    2 3
+    4 5 6
+    7 8 9 10
+    11 12 13 14 15
+    """
+    num = 1
+    for i in range(n):
+        for j in range(i + 1):
+            print(num, end=" ")
+            num += 1
+        print()
+
+#----------------- Incremental Alphabet Triangle -------------------------
+def incremental_alphabet_triangle(n):
+    """
+    A
+    B C
+    D E F
+    G H I J
+    K L M N O
+    """
+    alpha = ord('A')
+    for i in range(n):
+        for j in range(i + 1):
+            print(chr(alpha), end=" ")
+            alpha += 1
+        print()
+
+#----------------- Alphabet Triangle -------------------------
+def alphabet_triangle(n):
+    """
+    A
+    A B
+    A B C
+    A B C D
+    A B C D E
+    """
+    for i in range(n):
+        alphabet = ord('A')
+        for j in range(i + 1):
+            print(chr(alphabet), end=" ")
+            alphabet += 1
+        print()
+
+#----------------- Reverse Alphabet Triangle -------------------------
+def reverse_alphabet_triangle(n):
+    """
+    A B C D E
+    A B C D
+    A B C
+    A B
+    A
+    """
+    for i in range(n, 0, -1):
+        alphabet = ord('A')
+        for j in range(i):
+            print(chr(alphabet), end=" ")
+            alphabet += 1
+        print()
+
+#----------------- Repeated Alphabet Triangle -------------------------
+def repeated_alphabet_triangle(n):
+    """
+    A
+    B B
+    C C C
+    D D D D
+    E E E E E
+    """
+    letter = ord('A')
+    for i in range(n):
+        for j in range(i + 1):
+            print(chr(letter), end=" ")
+        letter += 1
+        print()
+
 
 
 
@@ -157,4 +274,24 @@ if __name__ == '__main__':
     print("\nCombined Patterns 3 and 4 (Diamond Shape):")
     diamond_shape_of_asterisks(n)
     
+    print("Alternating 0 and 1 Pattern:")
+    alternating_binary_pattern(n)
+    
+    print("\nNumber Pyramid with Spaces:")
+    number_pyramid_with_spaces(n)
+    
+    print("\nIncremental Number Triangle:")
+    incremental_number_triangle(n)
+    
+    print("\nIncremental Alphabet Triangle:")
+    incremental_alphabet_triangle(n)
+    
+    print("\nAlphabet Triangle:")
+    alphabet_triangle(n)
+    
+    print("\nReverse Alphabet Triangle:")
+    reverse_alphabet_triangle(n)
+    
+    print("\nRepeated Alphabet Triangle:")
+    repeated_alphabet_triangle(n)
     
