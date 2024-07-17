9d7cba1260872979be9c009a48934dfceb2beb38
Vedant Patil <137187890+VedantPatil-99@users.noreply.github.com>
2024-07-17 19:13:57 +0530

    Format Code

--- Learn The Basics/Know Basic Maths/isPalindrome.py
+++ Learn The Basics/Know Basic Maths/isPalindrome.py
@@ -1,12 +1,14 @@
 def isPalindrome(n):
-  rev = 0
-  copy = n
-  while copy > 0:
-    digit = copy % 10
-    rev = (rev * 10) + digit
-    copy //= 10
-  if n == rev:
-    print("It's a palindrome number.")
-  else:
-    print("It's NOT a palindrome number.")
-isPalindrome(131)
+    rev = 0
+    copy = n
+    while copy > 0:
+        digit = copy % 10
+        rev = (rev * 10) + digit
+        copy //= 10
+    if n == rev:
+        print(f"{n} is a palindrome number.")
+    else:
+        print(f"{n} is NOT a palindrome number.")
+
+n = int(input("Enter a number to check if it's a palindrome: "))
+isPalindrome(n)
