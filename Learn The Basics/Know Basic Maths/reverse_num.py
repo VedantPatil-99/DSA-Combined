ad2feb3d2178995952b128490dd26d4495f10d9d
Vedant Patil <137187890+VedantPatil-99@users.noreply.github.com>
2024-07-17 19:13:16 +0530

    Format Code

--- Learn The Basics/Know Basic Maths/reverse_num.py
+++ Learn The Basics/Know Basic Maths/reverse_num.py
@@ -1,9 +1,10 @@
 def reverse_number(n):
-  rev = 0
-  while n > 0:
-    digit = n % 10
-    rev = (rev * 10) + digit
-    n //= 10
-  return rev
-print("Reversed:", reverse_number(754))
-  
+    rev = 0
+    while n > 0:
+        digit = n % 10
+        rev = (rev * 10) + digit
+        n //= 10
+    return rev
+
+n = int(input("Enter a number to reverse it: "))
+print(f"Reversed {n}: {reverse_number(n)}")
