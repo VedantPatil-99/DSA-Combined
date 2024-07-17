a037b48ba4b8bd372fcfe13f35432155a4d77317
Vedant Patil <137187890+VedantPatil-99@users.noreply.github.com>
2024-07-17 19:07:55 +0530

    Format Code

--- Learn The Basics/Know Basic Maths/Count_Digits.py
+++ Learn The Basics/Know Basic Maths/Count_Digits.py
@@ -1,16 +1,23 @@
 def count_digits(n):
-  cnt = 0
-  while(n > 0):
-    lastdigit = n % 10
-    n //= 10
-    cnt += 1
-  return cnt
-digits = count_digits(1234)
-print(digits)
+    cnt = 0
+    while n > 0:
+        lastdigit = n % 10
+        n //= 10
+        cnt += 1
+    return cnt
+
+n = int(input("Enter a number to count its digits: "))
+digits = count_digits(n)
+print(f"Number of digits in {n}: {digits}")
+
 
 def print_digits(n):
-  while(n > 0):
-    lastdigit = n % 10
-    n //= 10
-    print(lastdigit, end = " ")
-print_digits(1234)
+    while n > 0:
+        lastdigit = n % 10
+        n //= 10
+        print(lastdigit, end=" ")
+    print()
+
+n = int(input("Enter a number to print its digits: "))
+print(f"Digits of {n}:")
+print_digits(n)
