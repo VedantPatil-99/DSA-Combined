fcbf8fcb4bc2bd9396421b23366c5861d629fd36
Vedant Patil <137187890+VedantPatil-99@users.noreply.github.com>
2024-07-20 23:36:40 +0530

    add Better Solution

--- Problems On Array/Easy/second_largest_element_in_arr.py
+++ Problems On Array/Easy/second_largest_element_in_arr.py
@@ -20,3 +20,34 @@ print("Second smallest element is:", Ans2)
 
 #------------------------------------------------
 
+# Better Solution
+
+def second_largest_n_smallest(arr, n):
+  if n == 0 or n == 1:
+    return (-1, -1)
+
+  small = float("inf")
+  sec_small = float("inf")
+  large = float("-inf")
+  sec_large = float("-inf")
+
+  for i in range(n):
+    small = min(small, arr[i])
+    large = max(large, arr[i])
+
+  for i in range(n):
+    if arr[i] < sec_small and arr[i] != small:
+      sec_small = arr[i]
+    if arr[i] > sec_large and arr[i] != large:
+      sec_large = arr[i]
+
+  return (sec_small, sec_large)
+
+
+arr = [12, 1234, 45, 67, 1]
+n = len(arr)
+Ans = second_largest_n_smallest(arr, n)
+print("Second smallest & largest element is:", Ans)
+
+#------------------------------------------------
+
