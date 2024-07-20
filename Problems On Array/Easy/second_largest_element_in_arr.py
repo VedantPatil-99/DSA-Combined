cd7e2e3a566b4eb58f78d7ab89cd36d46e473492
Vedant Patil <137187890+VedantPatil-99@users.noreply.github.com>
2024-07-20 23:43:06 +0530

    Update second_largest_element_in_arr.py
    Add Optimal Solution

--- Problems On Array/Easy/second_largest_element_in_arr.py
+++ Problems On Array/Easy/second_largest_element_in_arr.py
@@ -1,4 +1,6 @@
+
 # Brute Force Approach
+
 def second_largest(arr, n):
   arr.sort()
   return arr[n-2]
@@ -50,4 +52,41 @@ Ans = second_largest_n_smallest(arr, n)
 print("Second smallest & largest element is:", Ans)
 
 #------------------------------------------------
+
+# Optimal Solution 
+
+def second_smallest(arr, n):
+  small = float("inf")
+  sec_small = float("inf")
+
+  for i in range(n):
+    if arr[i] < small:
+      sec_small = small
+      small = arr[i]
+    elif arr[i] < sec_small and arr[i] != small:
+      sec_small = arr[i]
+
+  return sec_small
+
+def second_largest(arr, n):
+  large = float("-inf")
+  sec_large = float("-inf")
+
+  for i in range(n):
+    if arr[i] > large:
+      sec_large = large
+      large = arr[i]
+    elif arr[i] > sec_large and arr[i] != large:
+      sec_large = arr[i]
+
+  return sec_large
+
+arr = [12, 1234, 45, 67, 1]
+n = len(arr)
+Ans1 = second_smallest(arr, n)
+Ans2 = second_largest(arr, n)
+print("Second smallest element is:", Ans1)
+print("Second largest element is:", Ans2)
+
+
 
