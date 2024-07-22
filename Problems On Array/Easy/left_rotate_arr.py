4b118aa4ccac64e2b371a58014661bdccf178a3f
Vedant Patil <137187890+VedantPatil-99@users.noreply.github.com>
2024-07-22 23:50:52 +0530

    Update left_rotate_arr.py

--- Problems On Array/Easy/left_rotate_arr.py
+++ Problems On Array/Easy/left_rotate_arr.py
@@ -10,15 +10,15 @@ def left_rotate_one(arr, n):
         print(temp[i], end=" ")
     print()
 
-n = 5
-arr = [1, 2, 3, 4, 5]
-left_rotate_one(arr, n)
+arr1 = [1, 2, 3, 4, 5]
+n1 = len(arr1)
+left_rotate_one(arr1, n1)
 
 #-------------------------------------
 
 # Optimal Solution 
 
-def left_rotate_n(arr, n):
+def left_rotate_one(arr, n):
     temp = arr[0]
     for i in range(n - 1):
         arr[i] = arr[i + 1]
@@ -28,4 +28,4 @@ def left_rotate_n(arr, n):
 
 arr2 = [1, 2, 3, 4, 5]
 n2 = len(arr2)
-left_rotate_n(arr2, n2)
+left_rotate_one(arr2, n2)
